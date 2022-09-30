dfec0c5b1f3f62167ccada4d1a9978fb82c4bc8b
Kthwemoe2004 <kthwemoecoder2022@gmail.com>
2022-09-29 19:27:55 -0700

    second portfolio

---
+++ unuse.php
@@ -0,0 +1,42 @@
+            <li class="splide__slide">
+              <img src="images/alb2.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb3.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb4.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb5.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb6.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb7.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb8.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb9.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb10.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb11.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb12.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb13.jpg" class="img-fluid" alt="">
+            </li> 
+            <li class="splide__slide">
+              <img src="images/alb14.jpg" class="img-fluid" alt="">
+            </li>
+            <li class="splide__slide">
+              <img src="images/alb15.jpg" class="img-fluid" alt="">
+            </li>  
